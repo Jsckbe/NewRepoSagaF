1e2ae35157daa1e79f60497ea35b545d3eb114db
Jsckbe <u20201b254@upc.edu.pe>
2023-05-15 21:42:34 -0500

    Subiendo archivos de SagaFalabellaRetail al repo

---
+++ SagaFalabella/SagaFalabellaRetail/SagaFalabellaInicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SagaFalabellaRetail
+{
+    public partial class SagaFalabellaInicio : Form
+    {
+        public SagaFalabellaInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void SagaFalabellaInicio_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
